400d4209d0c11f6f55f65310682fd06a49e6b602
dazcalifornia <divertzt@gmail.com>
2023-12-09 07:17:37 +0700

    -

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </head>
       <body className={kanit.className}>
         <StyledComponentsRegistry>
           <ConfigProvider theme={theme}>
